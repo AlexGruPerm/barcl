9d1aea82e6bd7711ab0c8680d5b55f6b9d45b44e
AlexGruPerm <ugr@bk.ru>
2019-02-19 19:23:24 +0500

    Rewrite getCassSession to return Try[session] instead of Option[session]. Rewrite tests.

--- src/main/scala/casspackage/CassConnect.scala
+++ src/main/scala/casspackage/CassConnect.scala
@@ -4,28 +4,33 @@ import com.datastax.driver.core.exceptions.NoHostAvailableException
 import com.datastax.driver.core.{Cluster, Session}
 import org.slf4j.LoggerFactory
 
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Class just for opening Cassandra session and store it.
+  */
 class CassConnect {
 
-  object logger extends Serializable {
-    @transient lazy val log = LoggerFactory.getLogger(getClass.getName)
-  }
+  val logger = LoggerFactory.getLogger(getClass.getName)
 
-  def getCassSession(node_address : String) : Option[Session] = {
-    logger.log.debug("CassConnect.getCassSession - Try open connection to cassandra.")
+  def getCassSession(node_address : String) : Try[Session] = {
+    logger.debug("CassConnect.getCassSession - Try open connection to cassandra.")
     try {
       val session = Cluster.builder().addContactPoint(node_address).build().connect()
-      Option(session)
+      logger.debug("CassConnect.getCassSession - Connection opened.")
+      Success(session)
     } catch {
       case noHostAvlExc: NoHostAvailableException => {
-        logger.log.error("Host for connection is not available ["+node_address+"]")
-        None
+        val errMsg :String = "Host for connection is not available ["+node_address+"]"
+        logger.error(errMsg)
+        Failure(new AssertionError(errMsg))
       }
-      case _: Throwable => {
-        logger.log.error("Any kind of exception.")
-        None
+      case e: Throwable => {
+        val errMsg :String = "Any kind of exception :"+e.getMessage
+        logger.error(errMsg)
+        Failure(new AssertionError(errMsg))
       }
     }
-
   }
 
 }
--- src/test/scala/CassConnectTest.scala
+++ src/test/scala/CassConnectTest.scala
@@ -1,26 +1,31 @@
-import casspackage.{CassConnect}
+import casspackage.CassConnect
 import bcpackage.CassQueriesBinds
 import com.datastax.driver.core.Session
 import org.scalatest.FunSuite
 
+import scala.util.{Failure, Success, Try}
+
 class CassConnectTest extends FunSuite {
 
-  test("1. CassConnect.getCassSession Correct IP")
-    assert((new CassConnect).getCassSession("193.124.112.90").getOrElse(None) != None)
+  test("1. CassConnect.getCassSession Correct IP") {
+    assert((new CassConnect).getCassSession("193.124.112.90").isSuccess)
+  }
 
-  test("2. CassConnect.getCassSession Incorrect IP")
-    assert((new CassConnect).getCassSession("193.124.112.9").getOrElse(None) === None)
+  test("2. CassConnect.getCassSession Incorrect IP") {
+    assert((new CassConnect).getCassSession("193.124.112.9").isFailure)
+  }
 
   test("3. CassQueriesBinds created with closed session should throw IllegalArgumentException") {
     val node: String = "193.124.112.90"
-    val session : Option[Session] = (new CassConnect).getCassSession(node)
+    val session : Try[Session] = (new CassConnect).getCassSession(node)
     session match {
-      case Some(casSess) => {
-        casSess.close()
+      case Success(sess) => {
+        sess.close()
         intercept[IllegalArgumentException] {
-          new CassQueriesBinds(casSess)
+          new CassQueriesBinds(sess)
         }
       }
+      case Failure(f) => println("no test here")
     }
   }
 
