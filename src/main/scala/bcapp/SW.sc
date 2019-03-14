357864a2d99879a1dbaab61e1ebb0f6fbf87674f
AlexGruPerm <ugr@bk.ru>
2019-03-14 19:21:11 +0500

    Success and very fast loading ticks with batch, class TicksLoader.

--- src/main/scala/bcapp/SW.sc
+++ src/main/scala/bcapp/SW.sc
@@ -1,3 +1,14 @@
+
+
+val l :Seq[Int] = Seq(1,2,3,4,5,6,7,8,9,10)
+
+val sl = l.grouped(3)
+
+for (s <- sl){
+  println(s)
+}
+
+/*
 val keys = List(2, 5, 1)
 
 val mk :Map[Int,List[Int]] = Map(1->List(11,12,13), 2->List(21,22,23), 5->List(51,52,53))
@@ -7,3 +18,4 @@ val mk :Map[Int,List[Int]] = Map(1->List(11,12,13), 2->List(21,22,23), 5->List(5
 val nums1 = keys.map(x => mk.get(x))
 
 val nums2 = keys.flatMap(x => mk.get(x))
+*/
--- src/main/scala/bcapp/TicksLoader.scala
+++ src/main/scala/bcapp/TicksLoader.scala
@@ -1,7 +1,8 @@
 package bcapp
 
-import com.datastax.driver.core.{Cluster, LocalDate}
+import com.datastax.driver.core.{BatchStatement, Cluster, LocalDate}
 import org.slf4j.LoggerFactory
+
 import scala.collection.JavaConverters._
 
 /*
@@ -42,14 +43,26 @@ object TicksLoader extends App {
   val ClusterConfigTo = sessTo.getCluster.getConfiguration
   ClusterConfigTo.getSocketOptions.setConnectTimeoutMillis(60000)
 
+  val poolingOptionsFrom = sessFrom.getCluster.getConfiguration.getPoolingOptions
+  poolingOptionsFrom.setHeartbeatIntervalSeconds(90);
+
+  val poolingOptionsTo = sessTo.getCluster.getConfiguration.getPoolingOptions
+  poolingOptionsTo.setHeartbeatIntervalSeconds(90);
+
+  //------------------------------------------------------------------------------------
+
+
   val bndTicksCountDays = sessFrom.prepare(""" select ticker_id,ddate,ticks_count from mts_src.ticks_count_days """).bind()
 
   val bndTicksTotal = sessFrom.prepare(""" select * from mts_src.ticks_count_total """).bind()
 
   val bndTicksByTickerDdate = sessFrom.prepare(""" select * from mts_src.ticks where ticker_id=:tickerId and ddate=:dDate """).bind()
 
-  val bndSaveTick = sessTo.prepare(""" insert into mts_src.ticks(ticker_id,ddate,ts,db_tsunx,ask,bid)
-                                     	values(:p_ticker_id,:p_ddate,:p_ts,:p_db_tsunx,:p_ask,:p_bid) """).bind()
+  val bndSaveTickWb = sessTo.prepare(""" insert into mts_src.ticks(ticker_id,ddate,ts,db_tsunx,ask,bid)
+                                     	values(:p_ticker_id,:p_ddate,:p_ts,:p_db_tsunx,:p_ask,:p_bid) """)
+
+  //val bndSaveTick = sessTo.prepare(""" insert into mts_src.ticks(ticker_id,ddate,ts,db_tsunx,ask,bid)
+  //                                   	values(:p_ticker_id,:p_ddate,:p_ts,:p_db_tsunx,:p_ask,:p_bid) """).bind()
 
   val bndSaveTicksByDay = sessTo.prepare(""" update mts_src.ticks_count_days set ticks_count=ticks_count+:p_ticks_count where ticker_id=:p_ticker_id and ddate=:p_ddate """).bind()
 
@@ -85,6 +98,7 @@ object TicksLoader extends App {
     ).sortBy(t => t.db_tsunx)
     println(" READ sqTicks.size="+sqTicks.size+" head.db_tsunx="+sqTicks.head.db_tsunx)
 
+    /* Insert row by row. Slow.
     for (t <- sqTicks) {
       sessTo.execute(bndSaveTick
         .setInt("p_ticker_id", t.tickerId)
@@ -94,10 +108,25 @@ object TicksLoader extends App {
         .setDouble("p_ask",t.ask)
         .setDouble("p_bid",t.bid))
     }
+*/
 
-    sessTo.execute(bndSaveTicksCntTotal
-      .setInt("p_ticker_id", elm.ticker_id)
-      .setLong("p_ticks_count",tickCntTotal))
+    val partSqTicks = sqTicks.grouped(65535)
+
+    for(thisPartOfSeq <- partSqTicks)
+    {
+      var batch = new BatchStatement(BatchStatement.Type.UNLOGGED)
+      thisPartOfSeq.foreach {
+        t =>
+          batch.add(bndSaveTickWb.bind
+            .setInt("p_ticker_id", t.tickerId)
+            .setDate("p_ddate", t.dDate)
+            .setLong("p_ts", t.ts)
+            .setLong("p_db_tsunx", t.db_tsunx)
+            .setDouble("p_ask", t.ask)
+            .setDouble("p_bid", t.bid))
+      }
+      sessTo.execute(batch)
+    }
 
     sessTo.execute(bndSaveTicksByDay
       .setInt("p_ticker_id", elm.ticker_id)
@@ -105,6 +134,17 @@ object TicksLoader extends App {
       .setLong("p_ticks_count",elm.ticks_count))
 
     println("   TICKS SAVED INTO Cluster.")
+  }
+
+  /**
+    * Это вынесено отдельно т.к. на один тикер несколкьо дат.,
+    *
+    */
+  for(elm <- sqTicksCountDays.map(e => e.ticker_id).distinct) {
+    val tickCntTotal = sqTicksCountTotal.filter(tct => tct.ticker_id == elm).head.ticks_count
+    sessTo.execute(bndSaveTicksCntTotal
+      .setInt("p_ticker_id", elm)
+      .setLong("p_ticks_count",tickCntTotal))
   }
 
   sessFrom.close()
